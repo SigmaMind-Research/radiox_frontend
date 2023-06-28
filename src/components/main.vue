8269951395007379cf5248134ae4ab6a3381db0f
Jay S <jaidevsairwa@gmail.com>
2023-06-28 15:27:27 +0530

    issue=img-streach,cont-label,footerTools

--- src/components/main.vue
+++ src/components/main.vue
@@ -1,8 +1,29 @@
 <template>
-  <Sidebar @brig="bright" @cont="contrast" />
-  <v-main style="min-height:100vh" color="#252529">
-    <img :src="previewImage" :style="{ filter: 'brightness(' + bt + '%) ' + 'contrast(' + cont + '%)' }" alt=""
-      class="samimg">
+  <v-navigation-drawer color="#1e1e20" width="420">
+      <v-list>
+        <v-list-item>
+          <v-card id="card" variant="toned">
+            <h2 class="toolbr">Toolbar</h2>
+          </v-card>
+          <v-card id="card" title="Brightness" variant="toned">
+            <label id="lab" for="briVal" location="">{{ (briVal - 100).toFixed(1) }}</label>
+            <v-slider v-model="briVal" :max="200" track-color="white" color="white"
+              class="slid"></v-slider>
+          </v-card>
+          <v-card id="card" title="Contrast" variant="toned">
+            <label id="lab" for="briVal" location="">{{ (conVal - 100).toFixed(1) }}</label>
+            <v-slider v-model="conVal" :max="200" track-color="white" color="white"
+              class="slid"></v-slider>
+          </v-card>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  <!-- <Sidebar @brig="bright" @cont="contrast" /> -->
+  <v-main style="min-height:100vh; display: flex;" color="#252529">
+    <!-- <img :src="previewImage" :style="{ filter: 'brightness(' + bt + '%) ' + 'contrast(' + cont + '%)' }" alt=""
+      class="samimg"> -->
+          <img :src="previewImage" :style="{ filter: 'brightness(' + briVal + '%) ' + 'contrast(' + conVal + '%)' }" alt=""
+        class="samimg">
     <v-btn id="subb" variant="tonal" @click.stop="drawer = true" v-show="sub" location="bottom"
       color="primary">Submit</v-btn>
   </v-main>
@@ -22,7 +43,51 @@
     <Footer id="footerset" @close="viewTools = true" @setImg="imgset" />
   </div>
   <div v-if="viewTools">
-    <Tools id="footerset" @close="viewTools = false" @brig="bright" @cont="contrast" />
+    <!-- <Tools id="footerset" @close="viewTools = false" @brig="bright" @cont="contrast" /> -->
+    <v-bottom-navigation v-model="value" color="blue" class="overflow-visible" style="height: 56px;" grow>
+          <v-row justify="center">
+              <v-menu transition="scroll-y-reverse-transition">
+                  <template v-slot:activator="{ props }">
+                      <v-btn class="ma-2" v-bind="props" @click="viewTools=false">
+                          <v-icon icon="mdi-undo-variant" />
+                          Back
+                      </v-btn>
+                  </template>
+              </v-menu>
+              <v-menu transition="scroll-y-reverse-transition" width="300px" :close-on-content-click="false">
+                  <template v-slot:activator="{ props }">
+                      <v-btn class="ma-2" v-bind="props">
+                          <v-icon>mdi-brightness-6</v-icon>
+                          Brightness
+                      </v-btn>
+                  </template>
+                  <v-list>
+                      <v-list-item>
+                          <v-slider v-model="briVal" :max="200" track-color="white" color="white"
+                              class="slid" style="padding-top: 7px"></v-slider>
+                          <h3 style="text-align:center">Brigntness: {{ (briVal - 100).toFixed(1) }}</h3>
+                      </v-list-item>
+                  </v-list>
+              </v-menu>
+
+              <v-menu transition="scroll-y-reverse-transition" width="300px" :close-on-content-click="false">
+                  <template v-slot:activator="{ props }">
+                      <v-btn class="ma-2" v-bind="props">
+                          <v-icon icon="mdi-contrast-circle" />
+                          Contrast
+                      </v-btn>
+                  </template>
+                  <v-list>
+                      <v-list-item>
+                          <v-slider v-model="conVal" :max="200" track-color="white" color="white"
+                              class="slid" style="padding-top: 7px"></v-slider>
+                          <h3 style="text-align:center">Contrast: {{ (conVal - 100).toFixed(1) }}</h3>
+                      </v-list-item>
+                  </v-list>
+              </v-menu>
+          </v-row>
+
+      </v-bottom-navigation>
   </div>
 </template>
 
@@ -44,6 +109,9 @@ export default {
       bt: 100,
       cont: 100,
       sub: false,
+      briVal: 100,
+      conVal: 100,
+      value: '',
     }
   },
   components: {
@@ -58,28 +126,30 @@ export default {
       this.previewImage = name;
       this.sub = true;
     },
-    bright(value) {
-      this.bt = value * 2 + 20;
-      console.log(value * 2 + 20);
-    },
-    contrast(value) {
-      this.cont = value * 2 + 20;
-      console.log(value * 2 + 20);
-    }
+    // bright(value) {
+    //   this.bt = value * 2 + 20;
+    //   console.log(value * 2 + 20);
+    // },
+    // contrast(value) {
+    //   this.cont = value * 2 + 20;
+    //   console.log(value * 2 + 20);
+    // }
   },
   watch: {
     group() {
       this.drawer = false
     },
   },
+  emits:['close'],
   mixins: [bargrph],
 }
 </script>
 <style>
 .samimg {
   width: 100%;
-  height: 110%;
-  padding: 3px;
+  height:fit-content;
+   align-self: center;
+  padding: 10px;
 }
 
 #subb {
@@ -89,6 +159,29 @@ export default {
   padding: 0 30px;
 }
 
+#myFileInput {
+    display: none;
+}
+
+#card {
+  color: white;
+  background-color: #323643;
+  margin: 0px 5px 22px;
+}
+
+.slid {
+  padding: 30px 30px;
+}
+
+.toolbr {
+  text-align: center;
+  padding: 6px;
+}
+
+#lab {
+  margin-left: 80%;
+
+}
 @media screen and (min-width:1280px) {
   #footerset {
     display: none;
